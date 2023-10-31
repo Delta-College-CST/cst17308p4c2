da0cc433ce2bfa87aea30905e167924810bea1cc
teklingl <teklingl@gmail.com>
2023-10-31 15:27:09 -0400

    Examples/Practice

--- formatnumbersDONE.py
+++ formatnumbersDONE.py
@@ -1,7 +1,7 @@
 # This program formats phone numbers and social security numbers
 
 phone = "(989)-686-9140"
-ssn   = "1234567890"
+ssn   = "123456789"
 
 # Remove special characters from phone
 newphone = phone[1:4] + phone[6:9] + phone[10:]
